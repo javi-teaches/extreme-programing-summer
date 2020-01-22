8b7c49777fe5fec79908e1d7acfae89ca14c54c4
Ezequiel Cortés <codelando@gmail.com>
2020-01-22 19:56:44 -0300

    Agregamos las rutas iniciales

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send('Estoy en la página de inicio');
+});
+
+app.get('/acerca-de', (req, res) => {
+    res.send('Estoy en la página donde hablamos de nosotros');
+});
+
+app.get('/contacto', (req, res) => {
+    res.send('Estoy en la página de contacto');
+});
+
+app.get('*', (req, res) => {
+    res.send('Esa página no existe amiwo.');
+});
+
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
